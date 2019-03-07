f75ecfe890e65c4ad46dd61c321d9694cd5a66fd
Sebastian Horstmann <32675671+sh969@users.noreply.github.com>
2019-03-07 01:17:31 +0000

    Formatting

--- tests/cambike/sampler.py
+++ tests/cambike/sampler.py
@@ -6,7 +6,7 @@ Created on 7 Mar 2019
 @author: Sebastian Horstmann (sh969@cam.ac.uk)
 """
 
-# ads1115
+# ads1115 (taken from /scs_dfe_edu/tests/gas/ads1115_test.py)
 import time
 
 from scs_dfe.gas.ads1115 import ADS1115
@@ -14,11 +14,10 @@ from scs_dfe.gas.ads1115 import ADS1115
 from scs_host.bus.i2c import I2C
 from scs_host.sys.host import Host
 
-# OPC R1
+# OPC R1 (taken from scs_dfe_eng/tests/particulate/opc_r1_test.py)
 import sys
 
 from scs_core.data.json import JSONify
-from scs_core.sync.interval_timer import IntervalTimer
 
 from scs_dfe.particulate.opc_r1.opc_r1 import OPCR1
 
@@ -52,54 +51,54 @@ def read_conversion(device, channel):
 
 try:
     I2C.open(Host.I2C_SENSORS)
-	opc = OPCR1(Host.opc_spi_bus(), Host.opc_spi_device()
-	opc.power_on()
-	time.sleep(1)
-	opc.operations_on()
-	time.sleep(1)
+    opc = OPCR1(Host.opc_spi_bus(), Host.opc_spi_device())
+    opc.power_on()
+    time.sleep(1)
+    opc.operations_on()
+    time.sleep(1)
 
-	while 1:
-		# ads1115
-		wrk = ADS1115(ADS1115.ADDR_WRK, rate)
-		print("wrk: %s" % wrk)
+    while 1:
+        # ads1115
+        wrk = ADS1115(ADS1115.ADDR_WRK, rate)
+        print("wrk: %s" % wrk)
 
-		aux = ADS1115(ADS1115.ADDR_AUX, rate)
-		print("aux: %s" % aux)
-
-		no2_we_v = read_conversion(wrk, no2_we_channel)
-		print("no2_we_v: %0.6f" % no2_we_v)
-
-		no2_ae_v = read_conversion(aux, no2_ae_channel)
-		print("no2_ae_v: %0.6f" % no2_ae_v)
-
-		h2s_we_v = read_conversion(wrk, h2s_we_channel)
-		print("h2s_we_v: %0.6f" % h2s_we_v)
-
-		co_we_v = read_conversion(wrk, co_we_channel)
-		print("co_we_v: %0.6f" % co_we_v)
-
-		gnd_wrk_v = read_conversion(wrk, gnd_wrk_channel)
-		print("gnd_wrk_v: %0.6f" % gnd_wrk_v)
-
-		gnd_aux_v = read_conversion(aux, gnd_aux_channel)
-		print("gnd_aux_v: %0.6f" % gnd_aux_v)
-		
-		# opc r1
-		datum = opc.sample() 
-		print(JSONify.dumps(datum))
-		
-		# timing
-		now = time.time()
+        aux = ADS1115(ADS1115.ADDR_AUX, rate)
+        print("aux: %s" % aux)
+    
+        no2_we_v = read_conversion(wrk, no2_we_channel)
+        print("no2_we_v: %0.6f" % no2_we_v)
+    
+        no2_ae_v = read_conversion(aux, no2_ae_channel)
+        print("no2_ae_v: %0.6f" % no2_ae_v)
+    
+        h2s_we_v = read_conversion(wrk, h2s_we_channel)
+        print("h2s_we_v: %0.6f" % h2s_we_v)
+    
+        co_we_v = read_conversion(wrk, co_we_channel)
+        print("co_we_v: %0.6f" % co_we_v)
+    
+        gnd_wrk_v = read_conversion(wrk, gnd_wrk_channel)
+        print("gnd_wrk_v: %0.6f" % gnd_wrk_v)
+    
+        gnd_aux_v = read_conversion(aux, gnd_aux_channel)
+        print("gnd_aux_v: %0.6f" % gnd_aux_v)
+    		
+        # opc r1
+        datum = opc.sample() 
+        print(JSONify.dumps(datum))
+    		
+        # timing
+        now = time.time()
         print("interval: %0.3f" % round(now - checkpoint, 3))
-		checkpoint = now
-		time.sleep(5)
-		
-	sys.stdout.flush()
+        checkpoint = now
+        time.sleep(5)
+
+    sys.stdout.flush()
 
 except KeyboardInterrupt:
-	print("KeyboardInterrupt", file=sys.stderr)
+    print("KeyboardInterrupt", file=sys.stderr)
 		
 finally:
-	opc.operations_off()
-	opc.power_off()
-	I2C.close()
+    opc.operations_off()
+    opc.power_off()
+    I2C.close()

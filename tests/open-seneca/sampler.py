fe295ac671459131869712f9a1e9d23a434d4d60
Sebastian Horstmann <32675671+sh969@users.noreply.github.com>
2019-05-28 19:05:23 +0300

    Update sampler.py

--- tests/open-seneca/sampler.py
+++ tests/open-seneca/sampler.py
@@ -104,7 +104,7 @@ try:
         datum = opc.sample()
         # print(JSONify.dumps(datum))
         datum_dict = json.loads(JSONify.dumps(datum))
-        datum{"no2_we_v"} = no2_we_v
+        datum_dict{"no2_we_v"} = no2_we_v
         # print(datum_dict["pm1"])
         print(datum_dict)
     		

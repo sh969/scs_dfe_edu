9dc4e21c7c0a83a907131470890edbfc794bcde1
Sebastian Horstmann <32675671+sh969@users.noreply.github.com>
2019-06-06 00:23:51 +0100

    Update start_up.sh

--- start_up.sh
+++ start_up.sh
@@ -2,7 +2,6 @@
 
 # Update the code from Github if possible
 wget -q --spider http://google.com
-
 if [ $? -eq 0 ]; then
     echo "Online"
     cd /home/pi/
@@ -24,21 +23,26 @@ else
 fi
 
 echo "Changing hostname"
-
+# find imei of gsm module
 FILE=/home/pi/log/imei.txt
 if ! [ -f "$FILE" ]; then
     echo "IMEI" > $FILE
 fi
 imei=$(cat $FILE)
 
+# find git version number
 cd /home/pi/SCS/scs_dfe_edu/tests/open-seneca/
 version=$(git rev-list --count develop)
 
+# change hostname
 echo "os-"$imei"-"$version > hostname
 sudo mv -f hostname /etc
+# change hosts
+head -n -1 /etc/hosts > temp
+echo -e "127.0.0.1\t"$(cat /etc/hostname) >> temp
+sudo mv -f temp /etc/hosts
 
+# start script
 echo "Starting script"
-
 bash -c "export PYTHONPATH=/home/pi/SCS/scs_dev/src:/home/pi/SCS/scs_osio/src:/$"
-
 python3 ./sampler_gsm.py $version
